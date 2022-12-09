2ae3a74a0ff4ad9ac57c1eaf1c16eebe27cd4c77
Nikita Rogachevskiy <roagchevskiy2006@gmail.com>
2022-12-09 23:00:34 +0300

    sign-in

--- pages/page_reg/reg_sign_in.php
+++ pages/page_reg/reg_sign_in.php
@@ -1,15 +1,87 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Sign in</title>
+    <title>Students Registration</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link rel="stylesheet" href="test.css">
+    <link rel="stylesheet" href="../../index.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
-    <form action="reg_action.php" method="post">    
-        <input type="text" placeholder="Entar user name" name="user_name" required><br><br>
-        <input type="password" placeholder="Enter Password" name="psw" required><br><br>
-        <button type="submit" class="registerbtn" name="In" value="In">Sign in</button><br><br>
-    </form> 
   <body>
+    <div class="bg">
+
+      <div class="header">
+        <p class="logo">Sh.</p>
+      </div>
+
+      <br><br><br>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-4">
+          </div>
+          <div class="col-md-4" style="background: #010020; border-radius: 15px;">
+
+      <br><br><br><br><br><br><br>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-4">
+          </div>
+          <div class="col-md-4" style="background: #010020; border-radius: 15px;">
+
+            <br>
+            <p class="reg_heading">Войти</p>
+            <br>
+            <form style="text-align:center;">
+
+              <input class="reg_input" placeholder="Введите имя пользователя"><br><br><br>
+              <input class="reg_input" type="password" placeholder="Введите пароль"><br><br>
+
+              <a class="link_password" href="#">Забыли пароль?</a><br><br>
+
+              <form action="reg_test1.html" align="right">
+                <button class="button">Войти</button>
+              </form><br><br>
+
+            </form>
+          </div>
+
+          <div class="col-md-4">
+          </div>
+
+        </div>
+      </div>
+
+      <br><br><br><br><br><br><br>
+
+      <div class="container-fluid" style="background: #000000;">
+        <br><br>
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-2">
+            <p class="text3">Students help (c) 2022</p>
+            <p class="text4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div class="col-md-6">
+          </div>
+          <div class="col-md-3">
+            <p class="text3">Our contacts</p>
+            <p class="text4">tel: 8-800-555-35-35<br/>лучше позвOOOOOOOOOOOOOонить чем отсосать<br/>e-mail: her@pizda.hub<br/>t.me/hui_pizda</p>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+        <br><br>
+      </div>
+    </div>
   </body>
 </html>
