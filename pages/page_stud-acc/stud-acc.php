7983db7378003a385589805377f04bfb94bdf0c5
Nikita Rogachevskiy <roagchevskiy2006@gmail.com>
2022-12-09 23:22:09 +0300

    student-acc

---
+++ pages/page_stud-acc/stud-acc.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Students help</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="../../index.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="bg">
+
+      <div class="header">
+        <p class="logo">Sh.</p>
+        <a class="link" href="#">Вход</a>
+        <img class="photo" src="images/ava.jpg">
+      </div>
+
+      <br><br><br><br>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-4">
+            <p class="text5">У меня в работе</p>
+
+            <br>
+
+            <div style="background: url(images/1.jpg); background-size: cover; height: 600px; border-radius: 20px; position:relative; padding-bottom:50px;">
+              <div style="background: rgba(0, 0, 0, 0.5); position: absolute; bottom: 0px; border-radius: 0 0 20px 20px;">
+              <br>
+              <p class="text6" style="margin-left: 10%; width: 80%; ">Запрогать сайт</p>
+              <p class="text7" style="margin-left: 10%; width: 80%; ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+              <br>
+              </div>
+            </div>
+
+            <br>
+
+            <form action="pages/page_reg/reg_company.php" align="left">
+              <button class="button">Перейти к описанию</button>
+            </form>
+          </div>
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-1">
+            <div class="v2"></div>
+          </div>
+          <div class="col-md-4">
+            <p class="text5">Мои группы</p>
+
+            <br>
+
+            <div style="background: url(images/1.jpg); background-size: cover; height: 600px; border-radius: 20px; position:relative; padding-bottom:50px;">
+              <div style="background: rgba(0, 0, 0, 0.5); position: absolute; bottom: 0px; border-radius: 0 0 20px 20px;">
+              <br>
+              <p class="text6" style="margin-left: 10%; width: 80%; ">Студенты ИТМО</p>
+              <p class="text7" style="margin-left: 10%; width: 80%; ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+              <br>
+              </div>
+            </div>
+
+            <br>
+
+            <form action="pages/page_reg/reg_company.php" align="left">
+              <button class="button">Профиль группы</button>
+            </form>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+      </div>
+
+      <br><br><br><br>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-4">
+            <p class="text5">У меня в работе</p>
+
+            <br>
+
+            <div style="background: url(images/1.jpg); background-size: cover; height: 600px; border-radius: 20px; position:relative; padding-bottom:50px;">
+              <div style="background: rgba(0, 0, 0, 0.5); position: absolute; bottom: 0px; border-radius: 0 0 20px 20px;">
+              <br>
+              <p class="text6" style="margin-left: 10%; width: 80%; ">Запрогать сайт</p>
+              <p class="text7" style="margin-left: 10%; width: 80%; ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+              <br>
+              </div>
+            </div>
+
+            <br>
+
+            <form action="pages/page_reg/reg_company.php" align="left">
+              <button class="button">Перейти к описанию</button>
+            </form>
+          </div>
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-1">
+            <div class="v2"></div>
+          </div>
+          <div class="col-md-4">
+            <p class="text5">У меня в работе</p>
+
+            <br>
+
+            <div style="background: url(images/1.jpg); background-size: cover; height: 600px; border-radius: 20px; position:relative; padding-bottom:50px;">
+              <div style="background: rgba(0, 0, 0, 0.5); position: absolute; bottom: 0px; border-radius: 0 0 20px 20px;">
+              <br>
+              <p class="text6" style="margin-left: 10%; width: 80%; ">Запрогать сайт</p>
+              <p class="text7" style="margin-left: 10%; width: 80%; ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+              <br>
+              </div>
+            </div>
+
+            <br>
+
+            <form action="pages/page_reg/reg_company.php" align="left">
+              <button class="button">Перейти к описанию</button>
+            </form>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+      </div>
+
+      <br><br><br><br><br><br><br><br>
+
+      <div class="container-fluid" style="background: #000000;">
+        <br><br>
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-2">
+            <p class="text3">Students help (c) 2022</p>
+            <p class="text4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div class="col-md-6">
+          </div>
+          <div class="col-md-3">
+            <p class="text3">Our contacts</p>
+            <p class="text4">tel: 8-800-555-35-35<br/>лучше позвонить чем отсосать<br/>e-mail: her@pizda.hub<br/>t.me/hui_pizda</p>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+        <br><br>
+      </div>
+    </div>
+  </body>
+</html>
