078d87875cf612e663faa326cf564d1c8d9b442c
Nikita Rogachevskiy <roagchevskiy2006@gmail.com>
2022-12-10 14:30:36 +0300

    tabs

---
+++ pages/page_company-acc/company-acc.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Students help</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="../../index.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="bg">
+      <div class="header">
+        <p class="logo">Sh.</p>
+        <img class="img-responsive" style="width: 100%; height: 80px;" src="https://im.wampi.ru/2022/12/09/ij0LMGcdee5ff9805d8104.gif" alt="я джифка">
+        <a class="link" href="pages/page_reg/reg_sign_in.php">Вход</a>
+        <img class="photo" src="../../images/ava.jpg">
+      </div>
+
+      <br><br><br><br>
+
+      <div class="container">
+        <div class="tabs">
+          <nav class="tabs__items">
+            <a href="#tab_01" class="tabs__item"><span>Первая вкладка</span></a>
+            <a href="#tab_02" class="tabs__item"><span>Вторая вкладка</span></a>
+            <a href="#tab_03" class="tabs__item"><span>Третья вкладка</span></a>
+          </nav>
+          <div class="tabs__body">
+            <div id="tab_01" class="tabs__block">
+              Первая вкладка. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
+            </div>
+            <div id="tab_02" class="tabs__block">
+              Вторая вкладка. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
+            </div>
+            <div id="tab_03" class="tabs__block">
+              Третья вкладка. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
+            </div>
+          </div>
+        </div>
+        <div class="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
+        </div>
+      </div>
+
+      <div class="container-fluid" style="background: #000000;">
+        <br><br>
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-2">
+            <p class="text3">Students help (c) 2022</p>
+            <p class="text4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div class="col-md-6">
+          </div>
+          <div class="col-md-3">
+            <p class="text3">Our contacts</p>
+            <p class="text4">tel: 8-800-555-35-35<br/>лучше позвонить чем отсосать<br/>e-mail: her@pizda.hub<br/>t.me/hui_pizda</p>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+        <br><br>
+      </div>
+    </div>
+  </body>
+</html>
