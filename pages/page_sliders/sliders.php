b9d7e176c0b2dc88663bb7e698200e197e39d0e9
Sasha <nedakerrr@gmail.com>
2022-12-10 12:29:26 +0300

    new slide

--- pages/page_sliders/sliders.php
+++ pages/page_sliders/sliders.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html>
-
-  <div class="itc-slider" data-slider="itc-slider" data-loop="true" data-autoplay="true" data-interval="7000">
-    <div class="itc-slider__wrapper">
-      <div class="itc-slider__items">
-        <div class="itc-slider__item">
-          <img src="images/1.jpg" alt="...">
+  <head>
+    <link rel="stylesheet" href="../../modules/itc-slider/itc-slider.css">
+<!-- JavaScript -->
+    <script src="../../modules/itc-slider/itc-slider.js" defer></script>
+  </head>
+  <body>
+    <div class="itc-slider" data-slider="itc-slider" data-loop="true" data-autoplay="true" data-interval="7000">
+      <div class="itc-slider__wrapper">
+        <div class="itc-slider__items">
+          <div class="itc-slider__item">
+            <img src="../../images/1.jpg" alt="...">
+          </div>
+          <div class="itc-slider__item">
+            <img src="../../images/ava.jpg" alt="...">
+          </div>
+          <div class="itc-slider__item">
+            <img src="../../images/jr.jpg" alt="...">
+          </div>
         </div>
-        <div class="itc-slider__item">
-          <img src="images/ava.jpg" alt="...">
-        </div>
-        <div class="itc-slider__item">
-          <img src="images/jr.jpg" alt="...">
-        </div>
+        <ol class="itc-slider__indicators">
+          <li class="itc-slider__indicator" data-slide-to="0"></li>
+          <li class="itc-slider__indicator" data-slide-to="1"></li>
+          <li class="itc-slider__indicator" data-slide-to="2"></li>
+        </ol>
       </div>
-      <ol class="itc-slider__indicators">
-        <li class="itc-slider__indicator" data-slide-to="0"></li>
-        <li class="itc-slider__indicator" data-slide-to="1"></li>
-        <li class="itc-slider__indicator" data-slide-to="2"></li>
-      </ol>
+    <!-- Кнопки для перехода к предыдущему и следующему слайду -->
+      <button class="itc-slider__btn itc-slider__btn_prev"></button>
+      <button class="itc-slider__btn itc-slider__btn_next"></button>
     </div>
-  <!-- Кнопки для перехода к предыдущему и следующему слайду -->
-  <button class="itc-slider__btn itc-slider__btn_prev"></button>
-  <button class="itc-slider__btn itc-slider__btn_next"></button>
-</div>
+  </body>
 </html>
